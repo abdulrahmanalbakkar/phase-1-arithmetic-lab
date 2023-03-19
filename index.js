eecadc7623eef52eb41f75a98c2dc9d812e643cb
Abdulrahman <albakkar997@gmail.com>
2023-03-20 02:37:16 +0300

    Challange is done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Question 1:
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+
+//Question 2:
+let random = Math.ceil(Math.random()*1000);
+
+//Question 3:
+let num3 = 9;
+let num4 = 5
+let mod = num3 % num4;
+
+//Question 4:
+let max = Math.max(4, 8, 1, 20, 9, 16);
